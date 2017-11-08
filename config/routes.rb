651193ab48e138b2ca77445e28556a8a12782811
RTA <radkins95@aol.com>
2017-11-08 04:03:11 +0000

    blog app attempt 6

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts do
   	resources :comments
   end
-  get '/' => "rails/welcome#index"
+  get '/', to "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
